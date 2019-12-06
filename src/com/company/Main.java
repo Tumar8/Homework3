4093b98f31fb8bcb48e019b372c6525e14cc5b00
Tumar8 <tumarkasmalieva8@gmail.com>
2019-12-06 21:33:21 +0600

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,27 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        String[] names = new String[8]; //массив
-        names[1] = "Reina";
-        names[2] = "Sezimai";
-        names[3] = "Khanaiym";
-        System.out.println(names[1] + " " + names[2] + " " + names[3]);
+        String[] names = new String[3]; //массив
+        names[0] = "Reina";
+        names[1] = "Sezimai";
+        names[2] = "Khanaiym";
+        System.out.println(names[0] + " " + names[1] + " " + names[2]);
 
-        String name = ("names[1]");
-        String name1 = ("names[2]");
-        String name2 = ("names[3]");
-        switch (name) {
-            default:
-                System.out.println();
-        case ("names[1]"):
-                System.out.println("Good morning, " + names[1]);
-            case ("names[2]"):
-                System.out.println("Good afternoon, " + names[2]);
-            case ("names[3]"):
-                System.out.println("Good evening, " + names[3]);
+        for (int i = 0; i <names.length ; i++) {
+            switch (i) {
+                case 0:
+                    System.out.println("Good morning, " + names[0]);
+                    break;
+                case 1:
+                    System.out.println("Good afternoon, " + names[1]);
+                    break;
+                case 2:
+                    System.out.println("Good evening, " + names[2]);
+                    break;
 
+            }
         }
-
     }
 
 }
