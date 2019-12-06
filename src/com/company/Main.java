80c6f9c0ba7a0387aaae218ac68dbf0ce11416a5
Tumar8 <tumarkasmalieva8@gmail.com>
2019-12-06 20:58:35 +0600

    Homework3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,26 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
         String[] names = new String[8]; //массив
-        names[5] = "Reina";
-        names[7] = "Sezimai";
+        names[1] = "Reina";
+        names[2] = "Sezimai";
         names[3] = "Khanaiym";
-        System.out.println(names[5] + " " + names[7] + " " + names[3]);
+        System.out.println(names[1] + " " + names[2] + " " + names[3]);
 
-        String name = ("names[5]");
-        switch (name) {
-            case ("names[5]"):
-                System.out.println("Good morning, " + names[5]);
-        }
-        String name1 = ("names[7]");
-        switch (name1) {
-            case ("names[7]"):
-                System.out.println("Good afternoon, " + names[7]);
-        }
+        String name = ("names[1]");
+        String name1 = ("names[2]");
         String name2 = ("names[3]");
-        switch (name2) {
+        switch (name) {
+            default:
+                System.out.println();
+        case ("names[1]"):
+                System.out.println("Good morning, " + names[1]);
+            case ("names[2]"):
+                System.out.println("Good afternoon, " + names[2]);
             case ("names[3]"):
                 System.out.println("Good evening, " + names[3]);
+
         }
+
     }
 
 }
